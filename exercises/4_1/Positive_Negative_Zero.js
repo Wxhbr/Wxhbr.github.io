c05c2c2a7bd28754141a8fe9d361570909c89ac7
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2019-11-19 17:10:09 -0200

    Finalizado exercicios 2, 3, 4

---
+++ exercises/4_1/Positive_Negative_Zero.js
@@ -0,0 +1,8 @@
+let x = 0;
+if (x < 0){
+    console.log("O valor de x negativo " + x);
+} else if (x > 0){
+    console.log("O valor de x positvo " + x);
+} else {
+    console.log("O valor de x é zero " + x);
+}
