ae9fc8713525fbcacb982f71394d70275b8371c5
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2019-11-20 18:34:06 -0200

    Finalizado exericio 5 e uma parte do 4

--- exercises/4_2/imprimir_TrianguloRetInvertido_Asteriscos.js
+++ exercises/4_2/imprimir_TrianguloRetInvertido_Asteriscos.js
@@ -3,20 +3,12 @@ let stringAsteriscos = "";
 let string = "*";
 let stringTemp = "";
 
-//string = string.slice (0, 3) + stringTemp + string.slice(4)
-//console.log (string)
-
-
-
 if (n > 1){
     for (j=0; j < n; j++){
         stringAsteriscos = stringAsteriscos + " ";
     }
     console.log ("O valor escolhido é : " + n + "\n");
-    for (i = n; i >= 1; i--){        
-        
-        //stringTemp = stringAsteriscos.concat(string);
-        //console.log(stringAsteriscos.length)       
+    for (i = n; i >= 1; i--){    
         if ( i < n){
             stringTemp = stringTemp.slice (0, i-1) + string + stringTemp.slice (i);
         } else {
