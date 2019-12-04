467344c28ece9129a5c3c9289f27cc920a55e0af
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2019-12-04 15:40:44 -0200

    Adiconado from.html, script.js e style.css

---
+++ exercises/6.2/script.js
@@ -0,0 +1,31 @@
+function prevent(event) {    
+  event.preventDefault();
+}
+
+function validate(event) {
+  const reData = new RegExp('^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$');
+  const reEmail = new RegExp('^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$');
+  document.querySelectorAll('.validate').forEach(element => {
+    if (reEmail.test(element.value) || reData.test(element.value)) {
+        
+    } else {
+        
+    }
+  });
+}
+
+function criarDiv() {   
+}
+
+
+
+function startingEvents() {
+  document.querySelectorAll('.input').forEach(element => {
+    element.addEventListener('click', prevent);
+  });
+}
+
+window.onload = function load() {
+  document.querySelector('.btn').addEventListener('click', prevent);
+  document.querySelector('.btn').addEventListener('click', validate);
+}
